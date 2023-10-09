616e2e713079453c98a176f82367eeecd26b16b8
facr <alucar093@ieselcaminas.org>
2023-10-09 16:03:45 +0100

    actividad2.2

---
+++ src/Actividad2_2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Actividad2_2 {
+    public static void main(String[] args){
+        int a;
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce el numero: ");
+        a = sc.nextInt();
+
+
+
+
+        System.out.println("El numero es: " + (a >= 0));
+
+    }
+}
